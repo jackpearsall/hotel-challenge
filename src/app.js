61bcaaecd50d9a4cf0b72e2efe00343d7dea5457
Ian King <ian.antking@gmail.com>
2018-12-19 19:44:35 +0000

    set up user creation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const User = require('./models/users');
 
 const app = express();
+app.use(express.json());
+
+app.post('/user', (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+  });
+  user.save()
+    .then(() => {
+      res.status(201).json(user.toObject());
+    });
+});
 
 module.exports = app;
