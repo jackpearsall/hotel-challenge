cbe58eb24bf29729701378b45a7e47173100735a
Nevnet99 <luke-brannagan@hotmail.com>
2018-12-19 21:30:09 +0000

    prevented overbooking

--- src/app.js
+++ src/app.js
@@ -30,31 +30,52 @@ app.post('/hotel', (req, res) => {
     });
 });
 
+
+
 app.post('/booking', (req, res) => {
-  const booking = new Booking ({
-    startDate: req.body.startDate,
-    endDate: req.body.endDate,
-    user: req.body.userId,
+  Booking.find({
+    startDate: {
+      $gte: req.body.startDate,
+
+    },
+    endDate: {
+      $lte: req.body.endDate,
+    },
     hotel: req.body.hotelId,
-  });
-  booking.save()
-    .then(() => {
-      res.status(201).json(booking.toObject());
+  }, (error, bookings) => {
+
+    const roomsBooked = bookings.length;
+    Hotel.findById(req.body.hotelId, (error, hotel) => {
+      console.log(hotel.rooms, roomsBooked)
+      if (hotel.rooms <= roomsBooked) {
+        return res.status(400).json({ error: 'Hotel is fully booked.' });
+      }
+      const booking = new Booking({
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        user: req.body.userId,
+        hotel: req.body.hotelId,
+      });
+      booking.save()
+        .then(() => {
+          res.status(201).json(booking.toObject());
+        });
     });
+  });
 });
 
 app.patch('/booking/:id/accept', (req, res) => {
   Booking.findById(req.params.id, (err, booking) => {
     if (err || booking === null) {
-      res.status(404).json("Booking not found.")
+      res.status(404).json('Booking not found.');
     } else {
       booking.set({
         accepted: true,
-      })
+      });
       booking.save()
-      .then(() => {
-        res.status(200).json(booking.toObject());
-      }); 
+        .then(() => {
+          res.status(200).json(booking.toObject());
+        });
     }
   });
 });
