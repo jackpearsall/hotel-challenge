44675b0fd1b480f8227053c2a418e52891189eab
Michael Harrison <michael.harrison@peak.ai>
2018-12-20 12:27:00 +0000

    Fix merge conflicts

--- src/app.js
+++ src/app.js
@@ -30,15 +30,7 @@ app.post('/hotel', (req, res) => {
     });
 });
 
-
-
 app.post('/booking', (req, res) => {
-<<<<<<< HEAD
-  const booking = new Booking({
-    startDate: req.body.startDate,
-    endDate: req.body.endDate,
-    user: req.body.userId,
-=======
   Booking.find({
     startDate: {
       $gte: req.body.startDate,
@@ -47,26 +39,24 @@ app.post('/booking', (req, res) => {
     endDate: {
       $lte: req.body.endDate,
     },
->>>>>>> cbe58eb24bf29729701378b45a7e47173100735a
     hotel: req.body.hotelId,
-  }, (error, bookings) => {
-
+  }, (bookingsError, bookings) => {
     const roomsBooked = bookings.length;
-    Hotel.findById(req.body.hotelId, (error, hotel) => {
-      console.log(hotel.rooms, roomsBooked)
+    Hotel.findById(req.body.hotelId, (hotelError, hotel) => {
       if (hotel.rooms <= roomsBooked) {
-        return res.status(400).json({ error: 'Hotel is fully booked.' });
-      }
-      const booking = new Booking({
-        startDate: req.body.startDate,
-        endDate: req.body.endDate,
-        user: req.body.userId,
-        hotel: req.body.hotelId,
-      });
-      booking.save()
-        .then(() => {
-          res.status(201).json(booking.toObject());
+        res.status(400).json({ error: 'Hotel is fully booked.' });
+      } else {
+        const booking = new Booking({
+          startDate: req.body.startDate,
+          endDate: req.body.endDate,
+          user: req.body.userId,
+          hotel: req.body.hotelId,
         });
+        booking.save()
+          .then(() => {
+            res.status(201).json(booking.toObject());
+          });
+      }
     });
   });
 });
@@ -74,11 +64,7 @@ app.post('/booking', (req, res) => {
 app.patch('/booking/:id/accept', (req, res) => {
   Booking.findById(req.params.id, (err, booking) => {
     if (err || booking === null) {
-<<<<<<< HEAD
-      res.status(404).json('Booking not found.')
-=======
       res.status(404).json('Booking not found.');
->>>>>>> cbe58eb24bf29729701378b45a7e47173100735a
     } else {
       booking.set({
         accepted: true,
